4add8f67547ea591ec1e8e6db0255a72f5b6d614
freshcup <miketracz@hotmail.com>
2020-07-15 15:15:28 +0000

    implement AJAX

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,6 +1,9 @@
+const $submitButton = $('#submit');
 const $setLogin = $('#login');
 const $setSignUp = $('#signup');
-const $submitButton = $('#submit');
+const $emailInput = $('#email');
+const $passwordInput = $('#password');
+const $message = $('#message');
 
 let authSetting = 'login';
 
@@ -15,8 +18,45 @@ function setAuth(setting) {
     $setSignUp.addClass('active');
     $setLogin.removeClass('active');
     $submitButton.text('Sign Up');
-  } 
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
+
+  if (!email || !password) {
+    displayMessage('Email and password fields cannot be blank.', 'danger');
+    return;
+  }
+
+  $emailInput.val('');
+  $passwordInput.val('');
+
+  authenticateUser(email, password);
+}
+
+function displayMessage(message, type) {
+  $message.text(message).attr('class', type);
+}
+
+function authenticateUser(email, password) {
+  $.ajax({
+    url: '/' + authSetting,
+    data: {
+      user: {
+        email,
+        password
+      }
+    },
+    method: 'POST'
+  }).then(function(data) {
+    console.log(data);
+  });
 }
 
 $setLogin.on('click', setAuth.bind(null, 'login'));
 $setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handleFormSubmit);
