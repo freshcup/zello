195d2d7db30806ab2d10ac1358717d1126a6cff4
freshcup <miketracz@hotmail.com>
2020-07-15 14:14:01 +0000

    add home page login

---
+++ public/assets/js/home.js
@@ -0,0 +1,22 @@
+const $setLogin = $('#login');
+const $setSignUp = $('#signup');
+const $submitButton = $('#submit');
+
+let authSetting = 'login';
+
+function setAuth(setting) {
+  authSetting = setting;
+
+  if (authSetting === 'login') {
+    $setLogin.addClass('active');
+    $setSignUp.removeClass('active');
+    $submitButton.text('Log In');
+  } else {
+    $setSignUp.addClass('active');
+    $setLogin.removeClass('active');
+    $submitButton.text('Sign Up');
+  } 
+}
+
+$setLogin.on('click', setAuth.bind(null, 'login'));
+$setSignUp.on('click', setAuth.bind(null, 'signup'));
